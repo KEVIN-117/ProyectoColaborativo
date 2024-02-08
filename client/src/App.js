805b5c92513897190c33655793b5d6c4ed920224
Kevin Alexis Rodriguez Condori <sis414.kevinrodriguez17@gmail.com>
2024-02-08 17:02:46 -0400

    push frontend service

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,87 @@
 import {useState, useEffect} from "react"
 import './App.css';
-
+import axios from "axios"
 function App() {
   const [data, setData] = useState([]);
-
+  const [dataP, setDataP] = useState([]);
   useEffect(() => {
-    const data = fetch('http://localhost:3000/api/users/')
-    data.then(value => setData(value))
+    axios.get("http://localhost:3000/api/users").then(
+      res =>{
+        setData(res.data)
+      }
+    ).catch(
+      err => {
+        alert("Error", err  )
+      }
+    )
+    axios.get("http://localhost:3000/api/product").then(
+      res =>{
+        console.log(res.data)
+        setDataP(res.data)
+      }
+    ).catch(
+      err => {
+        alert("Error", err  )
+      }
+    )
   }, []);
   return (
     <div className="App">
-      
+      <table>
+        <thead>
+        <tr>
+          <th>id</th>
+          <th>name</th>
+          <th>lastName</th>
+          <th>created at</th>
+          <th>updated at</th>
+        </tr>
+        </thead>
+        <tbody>
+        {data.map((user, index) => (
+          <tr key={index} style={{
+            backgroundColor: user.id % 2 === 0 ? "red" : "blue"
+
+          }}>
+            <td>{user.id}</td>
+            <td>{user.name}</td>
+            <td>{user.last_name}</td>
+            <td>{user.createdAt}</td>
+            <td>{user.updatedAt}</td>
+          </tr>
+        ))}
+        </tbody>
+      </table>
+      <div></div>
+
+      <table>
+        <thead>
+        <tr>
+          <th>id</th>
+          <th>name</th>
+          <th>price</th>
+          <th>stock</th>
+          <th>created at</th>
+          <th>updated at</th>
+        </tr>
+        </thead>
+        <tbody>
+        {dataP.map((user, index) => (
+          <tr key={index}
+              style={{
+                backgroundColor: user.id % 2 === 0 ? "red" : "blue"
+              }}
+          >
+            <td>{user.id}</td>
+            <td>{user.name}</td>
+            <td>{user.price}</td>
+            <td>{user.stock}</td>
+            <td>{user.createdAt}</td>
+            <td>{user.updatedAt}</td>
+          </tr>
+        ))}
+        </tbody>
+      </table>
     </div>
   );
 }
--- src/app.js
+++ src/app.js
@@ -3,10 +3,13 @@ import morgan from 'morgan'
 import routerUser from './routes/users.routes.js'
 import routerProduct from './routes/product.route.js'
 
+import cors from 'cors'
+
 const app = express()
 
 app.use(express.json())
 app.use(morgan('dev'))
+app.use(cors())
 app.get('/', (req, res) => {
   res.send("hello world")
 })
